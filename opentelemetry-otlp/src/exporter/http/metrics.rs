122b664b851c64f34a0166f1cdd3f2dcb7cdd934
Christian Felipe Álvarez <calvarez@newrelic.com>
2023-09-19 16:52:33 +0200

    WIP

--- opentelemetry-otlp/src/exporter/http/metrics.rs
+++ opentelemetry-otlp/src/exporter/http/metrics.rs
@@ -33,10 +33,24 @@ impl MetricsClient for OtlpHttpClient {
             request.headers_mut().insert(k.clone(), v.clone());
         }
 
-        client
+        let response = client
             .send(request)
             .await
             .map_err(|e| MetricsError::ExportErr(Box::new(Error::RequestFailed(e))))?;
+
+        // TODO: use `opentelemetry_http::ResponseExt` instead (currently it returns TraceError)
+        if !response.status().is_success() {
+            let body_msg = std::str::from_utf8(response.body().iter().as_slice())
+                .unwrap_or("response body cannot be decoded");
+            return Err(MetricsError::ExportErr(Box::new(Error::RequestFailed(
+                format!(
+                    "request failed with status {} (Body: {})",
+                    response.status(),
+                    body_msg
+                )
+                .into(),
+            ))))?;
+        }
 
         Ok(())
     }
@@ -65,4 +79,56 @@ fn build_body(metrics: &mut ResourceMetrics) -> Result<(Vec<u8>, &'static str)> 
     Err(MetricsError::Other(
         "No http protocol configured. Enable one via `http-proto`".into(),
     ))
+}
+
+
+#[cfg(test)]
+mod tests {
+    // TODO: check if this tests could be implemented in a more generic way
+    use async_trait::async_trait;
+    use opentelemetry_http::{Bytes, HttpClient, HttpError, Request, Response};
+    use opentelemetry_sdk::{
+        metrics::{data::ResourceMetrics, exporter::PushMetricsExporter},
+        Resource,
+    };
+
+    #[derive(Debug, Default)]
+    struct MockClient {
+        response_bytes: Bytes,
+        status_code: http::StatusCode,
+    }
+
+    #[async_trait]
+    impl HttpClient for MockClient {
+        async fn send(&self, _: Request<Vec<u8>>) -> Result<Response<Bytes>, HttpError> {
+            let response = Response::<Bytes>::new(self.response_bytes.clone());
+            let (mut parts, body) = response.into_parts();
+            parts.status = self.status_code;
+            Ok(Response::from_parts(parts, body))
+        }
+    }
+
+    #[tokio::test]
+    async fn test_bad_status_code_error_message() {
+        let client = MockClient {
+            response_bytes: Bytes::from("Details"),
+            status_code: http::StatusCode::BAD_REQUEST,
+        };
+        let exporter = opentelemetry_otlp::new_exporter()
+            .http()
+            .with_http_client(client)
+            .build_metrics_exporter(
+                Box::new(opentelemetry_sdk::metrics::reader::DefaultAggregationSelector::new()),
+                Box::new(opentelemetry_sdk::metrics::reader::DefaultTemporalitySelector::new()),
+            )
+            .unwrap();
+        let mut metrics = ResourceMetrics {
+            resource: Resource::default(),
+            scope_metrics: vec![],
+        };
+        let result = exporter.export(&mut metrics).await.unwrap_err();
+        let debug_err = format!("{:?}", result);
+        assert!(debug_err.contains("400"));
+        assert!(debug_err.contains("Details"));
+    }
 }
